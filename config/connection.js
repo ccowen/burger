4e36f21d8730003008e9e2bba224292ce7c5d720
ccowen <caitlincowen2016@u.northwestern.edu>
2018-01-01 23:47:41 -0600

    screenshot, jawsdb code

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  //port: 3080,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "herokuconnection",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
